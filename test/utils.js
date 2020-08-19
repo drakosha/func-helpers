3fca2d228c6f32cbe2d1f4711ee276db94c38ca5
Mikhail Kostryukov <mike@triptrack.net>
2020-08-19 22:17:16 +0000

    Some tests added

---
+++ test/utils.js
@@ -0,0 +1,18 @@
+const assert = require('assert');
+const { getter } = require('..');
+
+describe('Utils', function () {
+  describe('getter', function () {
+    it('should return function like identity', function () {
+      assert.equal(getter()('test'), 'test');
+    });
+
+    it('should return getter function for path', function () {
+      assert.equal(getter('key1.key2')({ key1: { key2: 'test' } }), 'test');
+    });
+
+    it('should return undefined if no key found', function () {
+      assert.equal(getter('key1.key2')({ key5: { key3: 'test' } }), undefined);
+    });
+  });
+});
