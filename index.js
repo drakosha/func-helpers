ca2820f0d28addb0690df4b5fc056e9a83690c27
Mikhail Kostryukov <mike@triptrack.net>
2020-08-20 19:57:41 +0000

    When function added

--- index.js
+++ index.js
@@ -1,47 +1,7 @@
-'use strict';
+const when = require('./lib/when')
+const utils = require('./lib/utils');
 
 module.exports = {
-  mapKeys,
-  getter,
-  ensureFn,
-  forEachKey,
-  G: getter
+  ...utils,
+  when
 };
-
-const getterChache = {};
-
-function mapKeys(obj, callback) {
-  const result = [];
-
-  for (const key in obj) result.push(callback(key, obj[key]));
-
-  return result;
-}
-
-function ensureFn(obj, functionFactory = getter) {
-  if (typeof obj === 'function') return obj;
-
-  return functionFactory(obj);
-}
-
-function forEachKey(obj, callback) {
-  for (const key in obj) callback(key, obj[key]);
-}
-
-function getter(path) {
-  const cached = getterChache[path];
-  if (cached) return cached;
-
-  path = path ? path.split('.') : [];
-
-  return getterChache[path] = ctx => {
-    let result = ctx;
-
-    for (const key of path) {
-      if (!result) break;
-      result = result[key];
-    }
-
-    return result;
-  };
-}
